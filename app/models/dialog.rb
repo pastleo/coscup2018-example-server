836afb07680b3f41ad09612bab93c6568b2da107
蒼時弦也 <elct9620@frost.tw>
2018-07-18 10:48:08 +0800

    Add test for dialog

--- app/models/dialog.rb
+++ app/models/dialog.rb
@@ -4,4 +4,5 @@ class Dialog < ApplicationRecord
   belongs_to :mission, polymorphic: true
 
   validates :script, presence: true
+  validates :order, uniqueness: { scope: %i[mission_id mission_type] }
 end
